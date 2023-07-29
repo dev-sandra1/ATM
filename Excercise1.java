82aaf47993d73af168b769a921ec3069c4abf10b
Sandra Patiño <sandrapatiho@gmail.com>
2023-07-28 18:29:56 -0600

    secod commit

--- Excercise1.java
+++ Excercise1.java
@@ -3,29 +3,41 @@ import java.util.Scanner;
 public class Excercise1{
     public static void main(String[] args) {
         
-        Scanner enter = new Scanner(System.in);
+        Scanner entry = new Scanner(System.in);
+
         int startingSalary = 1000;
         int options;
-        int income, withdrawal,amount;
+        int result, take , withdrawal,amount;
 
-        System.out.println("Welcome to an automated teller machine, what do you want to do?:");
-        System.out.println(
-        "1. deposit money to the account\n" +
-        "2. withdraw money from the account\n" +
-        "3. go out");
-        options = enter.nextInt();
+        System.out.print("Welcome to an automated teller machine, what do you want to do?" 
+       + "\n 1. deposit money to the account" 
+       + "\n 2. withdraw money from the account" 
+       + "\n 3. go out"
+       + "\n option: ");
+        options = entry.nextInt();
+      
 
-        switch (options) {
-            case 1: income =Float.parseFloat(System.out.println("Type the amount you want to leave:")); 
-                 amount = startingSalary + income;
-          break;
+            switch (options) {
+                 case 1:  System.out.print("Type the amou nt you want to leave?: "); 
+                 amount = entry.nextInt();
+                  result = amount + startingSalary;
+                 System.out.print("your account total is: " + result);
+                  break;
+                  case 2:  System.out.print("how much do you want to withdraw?: ");
+                  withdrawal = entry.nextInt();
+                  take = startingSalary - withdrawal;
+                  System.out.print("your account total is: " + take);
+                  break;
+
+                 case 3: System.out.print("bye bye :D");
+                 break;
         
-         default:
-    
-          break;
+                 default:
+                System.out.print("please try again later:<");
+                 break;
 
 
-        }
+            }
 
 
     }
